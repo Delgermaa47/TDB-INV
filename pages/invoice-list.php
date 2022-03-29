bed21b504d0e9aeaef432c9330b9d78c230b4986
Delgermaa47 <sdelgermaa47@gmail.com>
2022-03-29 15:18:19 +0800

    static folder nemsen

--- pages/invoice-list.php
+++ pages/invoice-list.php
@@ -1,6 +1,6 @@
 <?php
     require ROOT."\inc\db.php";
-    $query = 'select * from users';
+    $query = 'select last_name from users';
     $result = _select($query, 'select_users');
     print_arr_values(json_decode($result));
 ?>
