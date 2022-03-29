7e5f179944cdef43534544eaf2974c8396131d24
Delgermaa47 <sdelgermaa47@gmail.com>
2022-03-29 16:15:11 +0800

    invoice list-iig gargasan

--- pages/invoice-list.php
+++ pages/invoice-list.php
@@ -1,5 +1,31 @@
 <?php
     require ROOT."\inc\db.php";
-    $query = 'select last_name from users';
-    $result = json_decode(_select($query, 'select_users'));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <ul>all user lists
+            <?php
+                $query = 'select * from users';
+                $results = json_decode(_select($query, 'select_users'), true);
+                foreach($results as $key=>$value) {
+                    extract($value);
+                    ?>
+                    <li><?php echo $id; ?></li>
+                    <li><?php echo $first_name; ?></li>
+                <?php
+                }
+                ?>
+        </ul>
+
+    </div>
+</body>
+</html>
