7cc67d7b4a2b1bd8128d0dd4f19167c699e4586b
Delgermaa47 <sdelgermaa47@gmail.com>
2022-03-29 15:37:43 +0800

    hmlt inhiretence hiih code-iig tvr butsaaw

--- pages/invoice-list.php
+++ pages/invoice-list.php
@@ -1,6 +1,5 @@
 <?php
     require ROOT."\inc\db.php";
     $query = 'select last_name from users';
-    $result = _select($query, 'select_users');
-    print_arr_values(json_decode($result));
+    $result = json_decode(_select($query, 'select_users'));
 ?>
---
+++ static/templates/main.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <script src="path/to/script"></script>
+    @yield('additional_scripts)
+</head>
+<body>
+    <div>This is some text I want to be shown in multiple views</div>
+    @yield('content')
+</body>
+</html>
