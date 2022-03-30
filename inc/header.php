820fa69dd7e384f365ee4e65754423f15d1733c0
Delgermaa47 <sdelgermaa47@gmail.com>
2022-03-30 21:28:06 +0800

    assets include hiij baigaa argiig uurchluh arga oloh

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/static/css/style.css">
+    <link rel="stylesheet" href="http://localhost/assets/static/css/style.css"> <!-- assets include hiihdee anhaarah--->
     <title>TDB inv </title>
 </head>
 <body>
