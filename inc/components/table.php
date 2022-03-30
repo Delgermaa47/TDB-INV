2dd72e6270979303e3edf4876a7fa29d2a81eb4a
Delgermaa47 <sdelgermaa47@gmail.com>
2022-03-30 15:13:51 +0800

    Table class-iig script deer duudsan

--- inc/components/table.php
+++ inc/components/table.php
@@ -8,7 +8,7 @@
         {
             $this->$propName = $propValue;
         }
-        
+
         public function diplay_table() {
             $table_class_name = $this->className ? $this->className : "table";
             $_header_className = get_or_null($this->header_details['class_name']);
@@ -20,7 +20,6 @@
             foreach($header_body_req as $key=>$value) {
                 extract($value);
                 $header_body = $header_body.'<th scope='.$scope.'class='.$className.'>'.$value.'</th>';
-
             }
 
             $table_body = '';
@@ -44,22 +43,4 @@
         }
 
     }
-
-    $employee = new NewTable();
-    $employee->className="table table-dark mt-4 pt-4";
-    $employee->header_details= json_decode('{
-        "class_name": "bg-dark text-white",
-        "header_data":[
-            {"field":"id", "value":"№", "className":"", "scope": " "},
-            {"field":"name", "value":"Name", "className":"", "scope": " "},
-            {"field":"phone", "value":"Phone Number", "className":"", "scope": " "}
-        ]
-    }', true);
-
-    $employee->body_datas= json_decode('[
-        {"id":" ", "name":"deegii", "phone":"999999"},
-        {"id":1, "name":" ", "phone":"999999"},
-        {"id":2, "name":"deegii", "phone":""}
-        ]', true);
-    $employee->diplay_table();
 ?>
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,20 @@
 <?php
-    require ROOT."\\inc\\components\\table.php";
+    require_once ROOT."\\inc\\components\\table.php";
+    $employee = new NewTable();
+    $employee->className="table table-dark mt-4 pt-4";
+    $employee->header_details= json_decode('{
+        "class_name": "bg-dark text-white",
+        "header_data":[
+            {"field":"id", "value":"№", "className":"", "scope": " "},
+            {"field":"name", "value":"Name", "className":"", "scope": " "},
+            {"field":"phone", "value":"Phone Number", "className":"", "scope": " "}
+        ]
+    }', true);
+
+    $employee->body_datas= json_decode('[
+        {"id":" ", "name":"deegii", "phone":"999999"},
+        {"id":1, "name":" ", "phone":"999999"},
+        {"id":2, "name":"deegii", "phone":""}
+        ]', true);
+    $employee->diplay_table();
 ?>
