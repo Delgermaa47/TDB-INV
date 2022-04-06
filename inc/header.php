1905a61758dd179b322037f5861fe00dca2f8c82
Delgermaa47 <sdelgermaa47@gmail.com>
2022-04-06 14:06:29 +0800

    popup gargadag bolson

--- inc/header.php
+++ inc/header.php
@@ -9,9 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://localhost/assets/static/css/style.css">
-    
     <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://localhost/assets/static/css/style.css">
     <title>TDB inv </title>
 </head>
 <body>
