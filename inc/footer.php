25a77d04b3d7862c0ce90d0f3bef2b532e300d63
Delgermaa47 <sdelgermaa47@gmail.com>
2022-04-07 09:12:00 +0800

    jquery-iin bolomjuudiig turshij vzsen

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,52 @@
+<?php
+    
+?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script>
+    function parseHTML(html) {
+        var t = document.createElement('template');
+        t.innerHTML = html;
+        return t.content;
+    }
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    function clicked(call_back) {
+        console.log("clicked")
+     
+//         console.log("id", typeof(id), id);
+//         var bla = `
+//             <div class="container">
+//                 <div class="popup text-primary">
+//                     <label class="h4 text-center mt-4">Хүлээн авагч</label>
+//                 </div>
+//             </div>
+//         `
+//         var bla2 = `
+//         <div class="modal" tabindex="-1" role="dialog">
+//   <div class="modal-dialog" role="document">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <h5 class="modal-title">Modal title</h5>
+//         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//           <span aria-hidden="true">&times;</span>
+//         </button>
+//       </div>
+//       <div class="modal-body">
+//         <p>Modal body text goes here.</p>
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//         `;
+//         var hoho = parseHTML(bla);
+        // var my_div = id.appendChild(hoho)
+    }
+
+</script>
 </body>
 </html>
