cca713baeb3ed2ae581f96dfb559818f25a95052
Delgermaa47 <sdelgermaa47@gmail.com>
2022-04-07 18:05:07 +0800

    frontend app nemsen

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+   <div>done</div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/invoice_list.jsx
@@ -0,0 +1,20 @@
+import React, {Component, Fragment} from 'react'
+
+
+export class InvoiceList extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state={
+        }
+    }
+
+    render() {
+        alert("done")
+        return (
+            <div className="row">
+               <label>bla</label>
+            </div>
+        );
+    }
+}
