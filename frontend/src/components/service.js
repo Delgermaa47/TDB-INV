1ec408150c6fdda0c6afdcdd6ac971f68ac3b7d3
Delgermaa47 <sdelgermaa47@gmail.com>
2022-04-08 10:32:54 +0800

    service js-iin aldaa zassan

--- frontend/src/components/service.js
+++ frontend/src/components/service.js
@@ -1,59 +1,8 @@
+import { handleResponse, getGetOptions } from "../inc/helpers/service"
 export const service = {
     detail_list,
     sub_detail_list
 }
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text)
-        if (!response.ok) {
-            if ([401, 403].indexOf(response.status) !== -1) {
-                // TODO auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                location.reload(true)
-            }
-            const error = (data && data.message) || response.statusText
-            return Promise.reject(error)
-        }
-
-        return data
-    })
-}
-
-function getGetOptions() {
-    return {
-        method: 'GET',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-        },
-    }
-}
-
-function getPostOptions() {
-    return {
-        method: 'POST',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-CSRFToken': getCookie('csrftoken'),
-        },
-    }
-}
-
 
 function detail_list(root_id, pk) {
     const opts = {...getGetOptions()}
--- frontend/src/inc/helpers/service.js
+++ frontend/src/inc/helpers/service.js
@@ -20,7 +20,7 @@ export function handleResponse(response) {
         if (!response.ok) {
             if ([401, 403].indexOf(response.status) !== -1) {
                 // TODO auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                location.reload(true)
+                window.location.reload(true)
             }
             const error = (data && data.message) || response.statusText
             return Promise.reject(error)
